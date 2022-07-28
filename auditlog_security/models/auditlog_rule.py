b09f5b8d8684e0fc953343dcdcc32746de1ee1cf
Giovanni Francesco Capalbo <giovanni@therp.nl>
2022-07-28 16:02:28 +0200

    [IMP] no more access errors, regressions, and methods to reduce number of logged fields

--- auditlog_security/models/auditlog_rule.py
+++ auditlog_security/models/auditlog_rule.py
@@ -3,7 +3,7 @@
 
 from odoo import exceptions, models, fields, api, modules, _, tools
 from odoo.addons.auditlog.models.rule import FIELDS_BLACKLIST
-
+from odoo.exceptions import ValidationError, UserError
 
 class AuditlogRule(models.Model):
     _inherit = "auditlog.rule"
@@ -12,11 +12,33 @@ class AuditlogRule(models.Model):
         "auditlog.line.access.rule", "auditlog_rule_id", ondelete="cascade"
     )
     server_action_id = fields.Many2one('ir.actions.server', "Server Action",)
+    log_selected_fields_only = fields.Boolean(
+        default=True,
+        help="Log only the selected fields, to save space avoid large DB data.")
+
+    @api.constrains('model_id')
+    def unique_model(self):
+        if self.search_count([('model_id', '=', self.model_id.id)]) > 1:
+            raise ValidationError("A rule for this model already exists")
+
+    @api.model
+    def get_auditlog_fields(self, model):
+        res = super(AuditlogRule, self).get_auditlog_fields(model)
+        unique_rule = self._get_rule_for_model(model)
+        if unique_rule.log_selected_fields_only:
+            traced_fields = unique_rule._get_fields_of_rule()
+            # we re-use the checks on non-stored fields from super.
+            return [x for x in traced_fields if x in res]
+        return res
 
     @api.multi
     def write(self, values):
-        if "auditlog_line_access_rule_ids" in values.keys():
-            #clear cache for all ormcache methods.
+        cache_invalidating_fields = [
+                "auditlog_line_access_rule_ids" ,
+                "log_selected_fields_only",
+        ]
+        if any([field in values.keys() for field in cache_invalidating_fields]):
+            # clear cache for all ormcache methods.
             self.clear_caches()
         return super(AuditlogRule, self).write(values)
 
@@ -25,6 +47,12 @@ class AuditlogRule(models.Model):
         # if model changes we must wipe out all field ids
         self.auditlog_line_access_rule_ids.unlink()
 
+    @tools.ormcache('model')
+    def _get_rule_for_model(self, model):
+        unique_rule =  self.env['auditlog.rule'].sudo().search(
+            [('model_id.model', '=', model._name)])
+        return unique_rule.sudo()
+
     @tools.ormcache('rule')
     def _get_fields_of_rule(rule):
         if rule.auditlog_line_access_rule_ids:
@@ -32,13 +60,14 @@ class AuditlogRule(models.Model):
                     'auditlog_line_access_rule_ids.field_ids').ids
         return []
 
-    @api.multi
-    def _get_view_log_lines_action(self):
+    @api.model
+    def _get_view_log_lines_action(self, rule_id=None):
+        rule = self.sudo().browse(rule_id)
         domain = [
-            ('model_id', '=', self.model_id.id),
+            ('model_id', '=', rule.model_id.id),
             ('res_id', 'in', self.env.context.get('active_ids')),
         ]
-        field_ids =  self._get_fields_of_rule() 
+        field_ids =  rule._get_fields_of_rule() 
         if field_ids:
             domain.append(('field_id', 'in', field_ids))
         lines = self.env['auditlog.log.line'].search(domain)
@@ -57,8 +86,7 @@ class AuditlogRule(models.Model):
     def _create_server_action(self):
         self.ensure_one()
         code = \
-            "rule = env['auditlog.rule'].browse(%s)\n" \
-            "action = rule._get_view_log_lines_action()" % (self.id)
+            "action =env['auditlog.rule']._get_view_log_lines_action(rule_id=%s)" % (self.id)
         server_action = self.env['ir.actions.server'].sudo().create({
             'name': "View Log Lines",
             'model_id': self.model_id.id,
